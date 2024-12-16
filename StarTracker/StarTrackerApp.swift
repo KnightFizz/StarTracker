18ec02a38cc0f37da1d806da804f720d02e704b6
Michael Fang <x20120533@163.com>
2024-12-16 15:27:15 +0800

    Initial Commit

---
+++ StarTracker/StarTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarTrackerApp.swift
+//  StarTracker
+//
+//  Created by Michael Fang on 16/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct StarTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
